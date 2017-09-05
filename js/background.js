25295b6480d7df551e3986ca370a66ae2a46b7fc
Ronaldo Araújo de Farias <rfarias@sp.gov.br>
2017-09-05 16:03:15 -0300

    Mantem 3 abas sempre abertas

--- js/background.js
+++ js/background.js
@@ -1,11 +1,38 @@
+function includes_list(lista, padrao){
+	var contem=false;
+	$(lista).each(function(){
+		if(this.includes(padrao) ) contem=true;		
+	});
+	return contem;
+	
+}
+
+
 $(document).ready(function(){
-    alert("working");
 	chrome.runtime.onMessage.addListener(function(msg){
 		console.log(msg);
 		
 	});
+    tab_urls=[];
+	setInterval(function(){
+		tab_urls=[];
+		chrome.tabs.query({},function(tabs){			
+			$(tabs).each(function(){
+				tab_urls.push(this.url);		
+			});	
 
+			if (!includes_list(tab_urls, '151014714C1_1_3') ) chrome.tabs.create({url:'https://mobile.365sport365.com/#type=Coupon;key=151014714C1_1_3;'});
+			if (!includes_list(tab_urls, '151017012C1_1_3') ) chrome.tabs.create({url:'https://mobile.365sport365.com/#type=Coupon;key=151017012C1_1_3;'});
+			if (!includes_list(tab_urls, 'MyBets') ) chrome.tabs.create({url:'https://mobile.365sport365.com/#type=MyBets;key=;ip=0;lng=1'});
+			
+		});		
+		
+	},1000)
+	
+	
 });
+
+
 
 
 
