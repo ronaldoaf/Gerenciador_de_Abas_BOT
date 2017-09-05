0845752477da5fb2b1041c4afd3bb30470e74cfc
Ronaldo Araújo de Farias <rfarias@sp.gov.br>
2017-09-05 16:38:57 -0300

    storage ainda não funcionando

--- js/background.js
+++ js/background.js
@@ -7,25 +7,33 @@ function includes_list(lista, padrao){
 	
 }
 
+var bot_ligado;
 
 $(document).ready(function(){
 	chrome.runtime.onMessage.addListener(function(msg){
 		console.log(msg);
 		
 	});
+	
     tab_urls=[];
 	setInterval(function(){
-		tab_urls=[];
-		chrome.tabs.query({},function(tabs){			
-			$(tabs).each(function(){
-				tab_urls.push(this.url);		
-			});	
-
-			if (!includes_list(tab_urls, '151014714C1_1_3') ) chrome.tabs.create({url:'https://mobile.365sport365.com/#type=Coupon;key=151014714C1_1_3;'});
-			if (!includes_list(tab_urls, '151017012C1_1_3') ) chrome.tabs.create({url:'https://mobile.365sport365.com/#type=Coupon;key=151017012C1_1_3;'});
-			if (!includes_list(tab_urls, 'MyBets') ) chrome.tabs.create({url:'https://mobile.365sport365.com/#type=MyBets;key=;ip=0;lng=1'});
-			
-		});		
+		
+		chrome.storage.sync.get('bot_ligado', function(obj) { 
+			bot_ligado=obj.bot_ligado;
+		});
+		
+		if (bot_ligado){
+			tab_urls=[];
+			chrome.tabs.query({},function(tabs){			
+				$(tabs).each(function(){
+					tab_urls.push(this.url);		
+				});	
+				if (!includes_list(tab_urls, '151014714C1_1_3') ) chrome.tabs.create({url:'https://mobile.365sport365.com/#type=Coupon;key=151014714C1_1_3;'});
+				if (!includes_list(tab_urls, '151017012C1_1_3') ) chrome.tabs.create({url:'https://mobile.365sport365.com/#type=Coupon;key=151017012C1_1_3;'});
+				if (!includes_list(tab_urls, 'MyBets') ) chrome.tabs.create({url:'https://mobile.365sport365.com/#type=MyBets;key=;ip=0;lng=1'});
+				
+			});		
+		}
 		
 	},1000)
 	
--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,34 @@
 
 $(document).ready(function() {
-	$('#cmn-toggle-1').prop('checked', true);
+	chrome.storage.sync.get('bot_ligado', function(obj) { 
+		if (obj.bot_ligado==true) $('#cmn-toggle-1').prop('checked', true);
+	});
+	
 
+	$("#cmn-toggle-1").change(function() {
+		if(!this.checked) {
+			chrome.storage.sync.get({bot_ligado: true});
+		}
+		else{
+			chrome.storage.sync.get({bot_ligado: false});
+		}
+	});
+	
+	
+	setInterval(function(){
+
+		
+		
+	},1000);
+	
+	
+	
+	/*
 	$('button').click(function(){
 		chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
 			
-		});
-		
-		
+		});		
 	});
-	
+	*/
 	
 });
