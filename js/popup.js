52babbcca2fcbb9a8f212dd9b814e9469f5a4448
Ronaldo Araújo de Farias <rfarias@sp.gov.br>
2018-12-27 17:02:35 -0200

    cartão vermelho e alteração nos vars

--- js/popup.js
+++ js/popup.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
 		if ($.isEmptyObject(result)) {			
 			config={		
 				minimo_indice_para_apostar:0.01,
-				percentual_de_kelly:0.5,
+				percentual_de_kelly:0.75,
 				maximo_da_banca_por_aposta:0.17
 			};		
 			chrome.storage.sync.set({config:config});		
