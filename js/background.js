8eea3533af9600840ba4bf788aa13a4c89c750c2
Ronaldo Araújo de Farias <rfarias@sp.gov.br>
2019-04-29 14:23:35 -0300

    simula android

--- js/background.js
+++ js/background.js
@@ -7,7 +7,7 @@ function includes_list(lista, padrao){
 	
 }
 
-/*
+
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details){
     for(var i=0; i < details.requestHeaders.length; ++i){
         if(details.requestHeaders[i].name === "User-Agent"){
@@ -19,7 +19,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function(details){
     return {requestHeaders: details.requestHeaders};
 }, {urls: ["<all_urls>"]}, ["blocking", "requestHeaders"]);
 
-*/
+
 
 
 
